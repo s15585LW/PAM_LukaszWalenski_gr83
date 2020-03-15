c97692ed04a2adb797448189e525cb417719bbc5
luw.dva <s15585@pjwstk.edu.pl>
2020-03-15 16:47:33 +0100

    pamo_cwiczenie_2

--- app/src/main/java/com/example/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bmi_calculator;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,57 +9,107 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.security.DomainCombiner;
+
 public class MainActivity extends AppCompatActivity {
-    EditText input1, input2;
-    TextView wynik, wynik_opis;
-    Button button;
- 
-    double BMI, wzrost, waga;;
+
+    Button btn_gotoBMI, btn_gotoDieta, btn_gotoKalorie, btn_Exit;
+    Double BMI_status, WAGA_status, WZROST_status, PLEC_status, WIEK_status;
+    TextView Txt_v_bmi;
+
+    private static final int SECOND_ACTIVITY_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input1 = (EditText) findViewById(R.id.editText);
-        input2 = (EditText) findViewById(R.id.editText2);
-        wynik = (TextView) findViewById(R.id.result);
-        button = (Button) findViewById(R.id.button);
-        wynik_opis = (TextView) findViewById(R.id.result2);
+        BMI_status = 0.0;
+        Txt_v_bmi = findViewById(R.id.BMI_txt2);
+        btn_gotoBMI = findViewById(R.id.btn_BMI);
+        btn_gotoDieta = findViewById(R.id.btn_dieta);
+        btn_gotoKalorie = findViewById(R.id.btn_Kalorie);
+        btn_Exit = findViewById(R.id.btn_Exit);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_gotoBMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                launchBMIActivity();
+            }
+        });
 
-                if (input1.getText().length() == 0 || input2.getText().length() == 0) {
-                    Snackbar.make(v, "Uzupełnij Dane", Snackbar.LENGTH_LONG)
+        btn_gotoDieta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (BMI_status == 0.0){
+                    Snackbar.make(v, "Oblicz swoje BMI", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                } else {
-                    wzrost = Integer.parseInt(input2.getText().toString());
-                    waga = Integer.parseInt(input1.getText().toString());
-
-                    if (wzrost == 0 || waga == 0) {
-                        Snackbar.make(v, "Wprowadź wartość większa od 0", Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show();
-                    } else {
-
-                        BMI = ((waga) /  ((wzrost * wzrost) / 10000));
-
-                        wynik.setText("BMI: " + String.format("%.2f", BMI));
-                        if (BMI < 18.5) {
-                            wynik_opis.setText("Niedowaga");
-                        } else if (BMI < 24.9) {
-                            wynik_opis.setText("Waga prawidłowa");
-                        } else if (BMI < 29.9) {
-                            wynik_opis.setText("Nadwaga");
-                        } else {
-                            wynik_opis.setText("Otyłość");
-                        }
-                    }
+                }else{
+                   launchDietaActivity();
                 }
+            }
+        });
+
+        btn_gotoKalorie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (BMI_status == 0.0){
+                    Snackbar.make(v, "Oblicz swoje BMI", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else{
+                    launchKalorieActivity();
+                }
+            }
+        });
+
+        btn_Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
             }
         });
 
     }
 
+    private void launchBMIActivity() {
+
+        Intent intent = new Intent(this, BMIActivity.class);
+        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
+
+    }
+
+    private void launchDietaActivity() {
+
+        Intent intent = new Intent(this, DietaActivity.class);
+        intent.putExtra("BMI_value", BMI_status);
+        startActivity(intent);
+    }
+
+    private void launchKalorieActivity() {
+
+        Intent intent = new Intent(this, KalorieActivity.class);
+        intent.putExtra("WAGA_value", WAGA_status);
+        intent.putExtra("WZROST_value", WZROST_status);
+        intent.putExtra("PLEC_value", PLEC_status);
+        intent.putExtra("WIEK_value", WIEK_status);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                BMI_status = data.getDoubleExtra("BMI_value", 0.0);
+                WAGA_status = data.getDoubleExtra("WAGA_value", 0.0);
+                WZROST_status = data.getDoubleExtra("WZROST_value", 0.0);
+                PLEC_status = data.getDoubleExtra("PLEC_value", 0.0);
+                WIEK_status = data.getDoubleExtra("WIEK_value", 0.0);
+
+                Txt_v_bmi.setText("Twoje BMI wynosi: " + String.format("%.2f", BMI_status));
+            }
+        }
+    }
 }
